c236b5c561ad6b471f77d8531f605cac3ac94c36
will <a10000005588@gmail.com>
2018-08-30 10:02:26 +0800

    fix(agent): add parameter:`blockNumber` in json-rpc api:getCode

--- modules/agent/historyUtils/getCode.go
+++ modules/agent/historyUtils/getCode.go
@@ -19,8 +19,10 @@ func GetCode(data rqst.Payload) interface{} {
 	var message string
 
 	params := data.Params
-	if len(params) != 1 {
-		message = fmt.Sprintf("Invalid params: invalid length 2, expected 1 elements in array.")
+	if len(params) != 2 {
+		// [contract address] [block number、"latest"、"earlest"、"pending"]
+		// We temporary forbid user to put "latest","earlest","pending" in parameters.
+		message = fmt.Sprintf("missing value for required argument.")
 		err = errors.New(message)
 		errors := common.Error{
 			ErrorType:        0,
@@ -28,8 +30,8 @@ func GetCode(data rqst.Payload) interface{} {
 		}
 		logger.Console().Panic(errors)
 	}
-	param := params[0]
-	contractAddress := param.(string)
+
+	contractAddress := params[0].(string)
 	contractAddress, err = historyUtilsCommon.Check40HeximalFormat(contractAddress)
 	if err != nil {
 		errors := common.Error{
@@ -39,22 +41,33 @@ func GetCode(data rqst.Payload) interface{} {
 		logger.Console().Panic(errors)
 	}
 
+	blockNumber := params[1].(string)
+	err = historyUtilsCommon.CheckBlockNumberFormat(blockNumber)
+	if err != nil {
+		errors := common.Error{
+			ErrorType:        0,
+			ErrorDescription: err.Error(),
+		}
+		logger.Console().Panic(errors)
+	}
+
 	fmt.Println("================getCode initial parameter================")
-	fmt.Printf("contractAddress:%s\n", contractAddress)
+	fmt.Printf("contractAddress:%s, blockNumber:%s \n", contractAddress, blockNumber)
 
 	var response rsps.GetCodeResponse
 
-	response = getCodeIndexer(contractAddress)
+	response = getCodeIndexer(contractAddress, blockNumber)
 	return response
 }
 
-func getCodeIndexer(contractAddress string) rsps.GetCodeResponse {
+func getCodeIndexer(contractAddress string, blockNumber string) rsps.GetCodeResponse {
 	var response rsps.GetCodeResponse
 	response.Jsonrpc = "2.0"
 	response.ID = 73
 
 	condition := bson.M{
 		"contractAddress": contractAddress,
+		"blockNumber":     blockNumber,
 	}
 
 	result, err := model.RetrieveContracts(condition)
